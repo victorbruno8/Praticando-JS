8344a5bcc187bcef2152331de66733ac7d8914fe
victorbruno8 <euvbruno8@gmail.com>
2025-03-27 18:14:21 -0300

    alteração

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,3 @@
-// const list = document.querySelector('ul');
-// const buttonForEach = document.querySelector('#foreach');
-// let myList = ''
-
-
-
-
-// buttonForEach.addEventListener('click', () => {
-
-//     menuOptions.forEach((product) => {
-//         myList +=  `
-
-//         <li>
-//                 <img src="${product.src}" alt="${product.name}">
-//                 <h2>${product.name}</h2>
-//                 <p>R$ ${product.price}</p>
-//         </li>
-
-//     `
-//     })
-
-//     list.innerHTML = myList
-// })
-
-
-
-// const list = document.querySelector('ul');
-
-// const buttonForEach = document.querySelector('#foreach');
-// const buttonMap = document.querySelector('#map');
-
-// buttonForEach.addEventListener('click', () => {
-//     list.innerHTML = ''; // Limpa a lista antes de adicionar os elementos novamente
-
-//     menuOptions.forEach((product, index) => {
-//         const li = document.createElement('li');
-//         li.classList.add('card'); // Classe para a animação
-
-//         li.innerHTML = `
-//             <img src="${product.src}" alt="${product.name}">
-//             <h2>${product.name}</h2>
-//             <p>R$ ${product.price}</p>
-//         `;
-
-//         list.appendChild(li);
-
-//         // Adiciona um pequeno delay para ativar a animação em sequência
-//         setTimeout(() => {
-//             li.classList.add('show');
-//         }, index * 150);
-//     });
-// });
-
-
-// buttonMap.addEventListener('click', () => {
-//     const newPrices = menuOptions.map((product) => ({
-      
-//             ...product,
-//             price: product.price * 0.9,
-       
-//     }));
-
-//     list.innerHTML = ''; // Limpa a lista antes de adicionar os elementos novamente
-
-//     newPrices.forEach((product) => {
-//         const li = document.createElement('li');
-//         li.classList.add('card'); // Classe para a animação
-
-//         li.innerHTML = `
-//             <img src="${product.src}" alt="${product.name}">
-//             <h2>${product.name}</h2>
-//             <p>R$ ${product.price}</p>
-//         `;
-
-//         list.appendChild(li);
-
-//         // Adiciona um pequeno delay para ativar a animação em sequência
-//         setTimeout(() => {
-//             li.classList.add('show');
-//         }, newPrices.indexOf(product) * 150);
-//     });
-
-// })
 
 
 const list = document.querySelector('ul');
@@ -95,7 +12,7 @@ function formatCurrency(value) {
 }
 
 function renderList(products) {
-    list.innerHTML = ''; // Limpa a lista antes de adicionar os elementos novamente
+    list.innerHTML = ''; 
 
     products.forEach((product, index) => {
         const li = document.createElement('li');
